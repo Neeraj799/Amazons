3c40fc8c56711e2a1174af994f33c991624ba3cf
neeraj <neerajpc799@gmail.com>
2023-09-08 00:59:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+        $("#banner").owlCarousel({
+            items: 1, // Number of items to display at a time (1 for a single image)
+            loop: true, // Infinite loop
+            autoplay: true, // Autoplay enabled
+            autoplayTimeout: 3000, // Autoplay interval in milliseconds (3 seconds)
+            autoplayHoverPause: true, // Pause on hover
+            dots: true // Show navigation dots
+        });
+    });
+
+
